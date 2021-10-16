4117820b60cc4f9fd14ede9ed412812a9106fc9a
AFK Redeem <afkredeem@gmail.com>
2021-10-17 01:56:58 +0300

    Fix drawer html from being constantly redrawn

--- lib/ui/components/drawer_links.dart
+++ lib/ui/components/drawer_links.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-
 import 'package:afk_redeem/data/consts.dart';
-import 'package:afk_redeem/data/services/afk_redeem_api.dart';
 import 'package:afk_redeem/ui/components/html_renderer.dart';
 import 'package:afk_redeem/ui/appearance_manager.dart';
 import 'package:afk_redeem/ui/image_manager.dart';
 
-Widget drawerLinks(BuildContext context, AfkRedeemApi afkRedeemApi) {
+Widget drawerLinks(BuildContext context, Future<String?> drawerHtml) {
   return Padding(
     padding: const EdgeInsets.symmetric(vertical: 10.0),
     child: Column(
@@ -68,11 +66,7 @@ Widget drawerLinks(BuildContext context, AfkRedeemApi afkRedeemApi) {
           ),
         ),
         FutureBuilder<String?>(
-            future: HtmlRenderer.getHtml(
-              context: context,
-              uri: kFlutterHtmlUri.drawer,
-              afkRedeemApi: afkRedeemApi,
-            ),
+            future: drawerHtml,
             builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
               if (!snapshot.hasData || snapshot.data == null) {
                 return Container();
--- lib/ui/screens/main_screen.dart
+++ lib/ui/screens/main_screen.dart
@@ -14,6 +14,7 @@ import 'package:afk_redeem/data/redemption_code.dart';
 import 'package:afk_redeem/data/services/afk_redeem_api.dart';
 import 'package:afk_redeem/data/user_message.dart';
 import 'package:afk_redeem/data/account_redeem_summary.dart';
+import 'package:afk_redeem/ui/components/html_renderer.dart';
 import 'package:afk_redeem/ui/components/about_dialog.dart';
 import 'package:afk_redeem/ui/components/disclosure_dialog.dart';
 import 'package:afk_redeem/ui/components/help_button.dart';
@@ -58,6 +59,11 @@ class _MainScreenState extends State<MainScreen>
   );
   double _brutusHeight = BASE_BRUTUS_HEIGHT;
   GlobalKey _brutusKey = GlobalObjectKey('brutus');
+  late Future<String?> _drawerLinks = HtmlRenderer.getHtml(
+    context: context,
+    uri: kFlutterHtmlUri.drawer,
+    afkRedeemApi: _afkRedeemApi,
+  );
 
   @override
   void initState() {
@@ -473,7 +479,7 @@ class _MainScreenState extends State<MainScreen>
                                     ],
                                   ),
                                   if (_canFetchHtmlPages)
-                                    drawerLinks(context, _afkRedeemApi),
+                                    drawerLinks(context, _drawerLinks),
                                   Padding(
                                     padding:
                                         const EdgeInsets.only(bottom: 10.0),
